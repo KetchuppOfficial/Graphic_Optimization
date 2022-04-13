8917ea8ddbacbeadab06e0b323f4b00882549d11
KetchuppOfficial <dolgov04@list.ru>
2022-04-13 23:10:08 +0300

    Initial commit

---
+++ Mandelbrot.cpp
@@ -0,0 +1,81 @@
+
+#include "../../TX/TXLib.h"
+
+#define VERT_SIZE 1080
+#define HOR_SIZE 1920
+
+int main (void)
+{
+    txCreateWindow (HOR_SIZE, VERT_SIZE); // creating window 1920x1080 px_iels
+    Win32::_fpreset();
+    txBegin();                 // блокирует обновление изображения окна во избежание миганий
+
+    typedef RGBQUAD (&scr_t) [VERT_SIZE][HOR_SIZE];
+    scr_t screen_buff = (scr_t) *txVideoMemory(); // буфер экрана
+
+    const int   n_max  = 256;
+    const float r_sq_max = 100.f;
+
+    float X_C = HOR_SIZE / 2.f + 300.f, Y_C = VERT_SIZE / 2.f, scale = 450.f; // координаты цента и коэффициент увеличения
+
+    for (;;)
+    {
+        if (GetAsyncKeyState (VK_ESCAPE)) // проверяет, нажата ли клавиша Esc
+            break;
+
+        if (txGetAsyncKeyState (VK_RIGHT)) 
+            X_C += ( txGetAsyncKeyState (VK_SHIFT) ? 100.f : 10.f );
+
+        if (txGetAsyncKeyState (VK_LEFT))  
+            X_C -= ( txGetAsyncKeyState (VK_SHIFT) ? 100.f : 10.f );
+
+        if (txGetAsyncKeyState (VK_DOWN))  
+            Y_C += ( txGetAsyncKeyState (VK_SHIFT) ? 100.f : 10.f );
+        
+        if (txGetAsyncKeyState (VK_UP))    
+            Y_C -= ( txGetAsyncKeyState (VK_SHIFT) ? 100.f : 10.f );
+        
+        if (txGetAsyncKeyState ('A'))      
+            scale += ( txGetAsyncKeyState (VK_SHIFT) ? 100.f : 10.f );
+        
+        if (txGetAsyncKeyState ('Z'))      
+            scale -= ( txGetAsyncKeyState (VK_SHIFT) ? 100.f : 10.f );
+
+        for (int y_i = 0; y_i < VERT_SIZE; y_i++) 
+        {
+            if (GetAsyncKeyState (VK_ESCAPE)) 
+                break;
+ 
+            float y_0 = ((float)y_i - Y_C) / scale;
+
+            for (int x_i = 0; x_i < HOR_SIZE; x_i++)
+            {                                                                                        
+                float x_0 = ((float)x_i - X_C) / scale;
+
+                int N = 0;
+                for (float x = x_0, y = y_0; N < n_max; N++)
+                {
+                    float x_sq = x * x, y_sq = y * y, xy = x * y;
+
+                    float r_sq = x_sq + y_sq;
+                    if (r_sq >= r_sq_max)
+                        break;
+                        
+                    x = x_sq - y_sq + x_0;
+                    y = xy + xy + y_0;
+                }
+                
+                float I = sqrtf (sqrtf ((float)N / (float)n_max)) * 255.f;
+
+                BYTE c = (BYTE) I;
+                RGBQUAD color = (N < n_max) ? RGBQUAD { (BYTE) (255 - c), (BYTE) (c%2 * 64), c } : RGBQUAD {};
+                screen_buff[y_i][x_i] = color;
+            }
+        }
+            
+        printf ("\t\r%.flf", txGetFPS());
+        txUpdateWindow(); // разрешить обновление экрана
+    }
+
+    txDisableAutoPause(); // txIDontWantToHaveAPauseAfterMyProgramBeforeTheWindowWillClose_AndIWillNotBeAskingWhereIsMyPicture ()
+}
